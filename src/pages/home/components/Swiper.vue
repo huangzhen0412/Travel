c5826d7c9042614f539202f423e4974596876714
huangzhen <305325375@qq.com>
2018-05-08 18:47:59 +0830

    add swiper and icons

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,40 @@
+<template>
+	<swiper :options="swiperOption">
+    	<!-- slides -->
+    	<swiper-slide v-for="item of swiperList" :key="item.id">
+    		<img class="swiper-img" :src="item.imgUrl">
+    	</swiper-slide>
+    	<!-- Optional controls -->
+    	<div class="swiper-pagination"  slot="pagination"></div>
+  	</swiper>
+</template>
+
+<script>
+	export default {
+		name: 'HomeSwiper',
+		data() {
+			return {
+				swiperOption: {
+					pagination: '.swiper-pagination'
+				},
+				swiperList: [
+					{
+						id: 1,
+						imgUrl: 'https://img1.qunarzz.com/qs/1805/66/29a40eeb420e0302.jpg'
+					},
+					{
+						id: 2,
+						imgUrl: 'https://img1.qunarzz.com/qs/1805/a9/f01d076cea2ee902.jpg'
+					}
+				]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	>>> .swiper-pagination-bullet-active
+		background: #fff
+	.swiper-img
+		width: 100%
+</style>
